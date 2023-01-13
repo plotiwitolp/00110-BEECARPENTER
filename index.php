7195840fc0c314ec3b77b725b446e8f02e69a3fe
plotiwitolp <plotiwitolp@gmail.com>
2023-01-13 20:48:04 +0400

    start home page

---
+++ index.php
@@ -0,0 +1,272 @@
+<!DOCTYPE html>
+<html lang="ru">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Bee Carpenter | Разработка сайтов</title>
+  <link rel="stylesheet" href="./assets/styles/main.css">
+</head>
+
+<body>
+  <div class="page-wrapper">
+
+    <!-- header -->
+    <header>
+      <div class="header">
+        <div class="header__left">
+          <a href="#">
+            <img src="./assets/images/LOGO-v1.png" alt="" class="header__logo">
+          </a>
+        </div>
+        <div class="header__right">
+          <nav class="header__nav">
+            <ul class="header__ul">
+              <li class="header__item neo"><a href="#">главная</a></li>
+              <li class="header__item neo"><a href="#">о нас</a></li>
+              <li class="header__item neo"><a href="#">портфолио</a></li>
+              <li class="header__item neo"><a href="#">услуги</a></li>
+              <li class="header__item neo"><a href="#">блог</a></li>
+              <li class="header__item neo"><a href="#">контакты</a></li>
+            </ul>
+          </nav>
+          <div class="socials">
+            <div class="socials__item neo">
+              <a href="#">
+                <img class="social__telegram" src="./assets/images/telegram.png" alt="">
+              </a>
+            </div>
+            <div class="socials__item neo">
+              <a href="#">
+                <img class="social__whatsapp" src="./assets/images/whatsapp.png" alt="">
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </header>
+
+    <!-- main -->
+    <main>
+      <!-- top-banner -->
+      <div class="top-banner">
+        <div class="top-banner__left">
+          <div class="top-banner-ava">
+            <div class="top-banner-ava__pic">
+              <img src="./assets/images/ava-nadya-v2.png" alt="">
+            </div>
+            <div class="top-banner-ava__desc neo">
+              <div class="top-banner-ava__name">
+                NADEZHDA
+              </div>
+              <div class="top-banner-ava__spec">
+                WEB-DEVELOPER
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="top-banner__middle">
+          <h1>
+            <span class="blue">разработка</span>
+            <span class="gray">&</span>
+            <span class="pink">дизайн</span>
+            <span class="black">сайтов</span>
+          </h1>
+          <div class="order_btn neo">заказать сайт</div>
+        </div>
+        <div class="top-banner__right">
+          <div class="top-banner-ava">
+            <div class="top-banner-ava__pic">
+              <img src="./assets/images/ava-vanya-v2.png" alt="">
+            </div>
+            <div class="top-banner-ava__desc neo">
+              <div class="top-banner-ava__name">
+                IVAN
+              </div>
+              <div class="top-banner-ava__spec">
+                WEB-DEVELOPER
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <!-- SUCCESS -->
+      <div class="dbl-block">
+        <div class="dbl-block__text neo">
+          <div class="dbl-block__shadow-text">SUCCESS</div>
+          <h2>
+            <span class="blue">ВАША ИДЕЯ +</span>
+            <span class="pink">НАША РЕАЛИЗАЦИЯ</span>
+            <span class="blue">= УСПЕХ БИЗНЕСА</span>
+          </h2>
+          <div class="dbl-block__desc">
+            <p>
+              Для нас важна каждая деталь. Поэтому, если вы решили увеличить колличество клиентов, повысить лояльность ваших покупателей или получать больше посетителей, то вот три шага, чтобы понимать из чего состоит сайт и с чего начать.
+            </p>
+          </div>
+          <span class="arrow-1"></span>
+        </div>
+        <div class="dbl-block__pic neo">
+          <img src="./assets/images/img-7.png" alt="">
+        </div>
+      </div>
+
+      <!-- STEP 1 -->
+      <div class="dbl-block">
+        <div class="dbl-block__pic neo">
+          <img src="./assets/images/img-2.png" alt="">
+        </div>
+        <div class="dbl-block__text neo">
+          <div class="dbl-block__shadow-text">STEP 1</div>
+          <h3>
+            Шаг 1. Грамотный сайт - это совокупность современного <span class="pink">дизайна</span>, правильной <span class="blue">верстки</span>, удобной <span class="blue">системы управления контентом</span> и <span class="black">продвижение</span>.
+          </h3>
+          <div class="dbl-block__desc">
+            <p>
+              Что касается правильной верстки, реализации удобной системы управления контентом, а также дизайна, то это те самые области, в которых мы лучше всего разбираемся.
+            </p>
+            <p>
+              Насчет остальных областей, мы тоже имеем определенный опыт и общее понимание. Сможем подсказать базовые правила по наполнению и продвижению сайта, а также где что искать.
+            </p>
+            <p>
+              Но чтобы что-то наполнять и продвигать, сначала должно быть это что-то. Поэтому вам нужен сам сайт, соответственно перейдем к следующему шагу.
+            </p>
+          </div>
+          <span class="arrow-2"></span>
+        </div>
+      </div>
+
+      <!-- STEP 2 -->
+      <div class="dbl-block">
+        <div class="dbl-block__text neo">
+          <div class="dbl-block__shadow-text">STEP 2</div>
+          <h3>
+            Шаг 2. С чего же начать?
+          </h3>
+          <div class="dbl-block__desc">
+            <p>
+              Перед тем как взяться за реализацию сайта, составляется техническое задание (ТЗ). Оно не обязательно должно быть составлено по каким-либо правилам или иметь какое-то специальное оформление. Это может быть просто обычный текстовый документ или даже листок бумаги.
+            </p>
+            <p>
+              В ТЗ должно быть то, что вы хотите видеть на своем сайте: страницы, блоки на этих страницах, и их расположение (текста, картинки, видео). Или можете просто указать ссылки с сайтами-примерами. Всё что считаете нужным, что поможет посетителю сайта заинтересоваться вашим товаром или услугой.
+            </p>
+            <p>
+              После того, как будет готово ТЗ, мы приступим к выполнению вашего проекта, но перед этим мы оговорим с вами организационные моменты. Поэтому перейдем к последнему шагу.
+            </p>
+          </div>
+          <span class="arrow-3"></span>
+        </div>
+        <div class="dbl-block__pic neo">
+          <img src="./assets/images/img-3.png" alt="">
+        </div>
+      </div>
+
+      <!-- STEP 3 -->
+      <div class="dbl-block">
+        <div class="dbl-block__pic neo">
+          <img src="./assets/images/img-4.png" alt="">
+        </div>
+        <div class="dbl-block__text neo">
+          <div class="dbl-block__shadow-text">STEP 3</div>
+          <h3>
+            Шаг 3. Организационные моменты и оплата.
+          </h3>
+          <div class="dbl-block__desc">
+            <p>
+              Проект обычно разбивается на несколько этапов в зависимости от сложности. Оговариваем с вами сроки и стоимость.
+            </p>
+            <p>
+              Перед началом выполнения первого этапа, мы получаем от вас предоплату в размере 50% от стоимости этой части. Если после реализации первого этапа вас все устраивает, то мы получаем вторые 50% и переходим к выполнению следующей части. Если будут каки-либо недочеты и они будут в рамках ТЗ, то мы исправляем и передаем вам на проверку. До тех пор пока первая часть не будет завершена.
+            </p>
+            <p>
+              Также есть вариант оформить заказ сайта через фриланс биржу (с арбитражем).
+            </p>
+            <p>
+              Далее беремся за следующую часть по такому же принципу до тех пор пока весь проект не будет полностью готов.
+            </p>
+          </div>
+        </div>
+      </div>
+
+      <!-- feedback-form -->
+      <div class="feedback-form neo">
+        <div class="feedback-form__shadow-text">ПОЖУЖЖИМ С ЛЮБОВЬЮ НАД ВАШИМ САЙТОМ</div>
+        <div class="feedback-form__body">
+          <span class="feedback-form__title">Let’s go:</span>
+          <form action="">
+            <div class="feedback-form__item">
+              <div class="feedback-form__label">Здравствуйте! Меня зовут</div><input type="text" class="feedback-form__input">
+            </div>
+            <div class="feedback-form__item">
+              <div class="feedback-form__label">И я хочу сайт на тему</div><input type="text" class="feedback-form__input">
+            </div>
+            <div class="feedback-form__item">
+              <div class="feedback-form__label">Мой контактный номер</div><input type="number" class="feedback-form__input">
+            </div>
+            <div class="feedback-form__item">
+              <div class="feedback-form__label">Мне бы хотелось дополнить ещё:</div>
+              <textarea placeholder="__________________________" class="feedback-form__input"></textarea>
+            </div>
+            <div class="feedback-form__bottom">
+              <div class="feedback-form__person-data">
+                <label for="person-data" class="feedback-form__person-data-label">
+                  <input type="checkbox" name="person-data" id="person-data">
+                  <span class="feedback-form__person-data-check neo"></span>
+                </label>
+                <span class="feedback-form__person-data-text">
+                  Я согласен с <a href="#">обработкой моих персональных данных</a>
+                </span>
+              </div>
+              <input class="feedback-form__submit neo" type="submit" value="ОТПРАВИТЬ">
+            </div>
+          </form>
+        </div>
+      </div>
+
+    </main>
+
+    <!-- footer -->
+    <footer class="footer neo">
+      <div class="footer__top">
+
+        <div class="footer__nav">
+          <ul class="footer__ul">
+            <li class="footer__item neo"><a href="#">главная</a></li>
+            <li class="footer__item neo"><a href="#">о нас</a></li>
+            <li class="footer__item neo"><a href="#">портфолио</a></li>
+            <li class="footer__item neo"><a href="#">услуги</a></li>
+            <li class="footer__item neo"><a href="#">блог</a></li>
+            <li class="footer__item neo"><a href="#">контакты</a></li>
+          </ul>
+        </div>
+
+        <div class="footer__socials">
+          <div class="socials">
+            <div class="socials__item neo">
+              <a href="#">
+                <img class="social__telegram" src="./assets/images/telegram.png" alt="">
+              </a>
+            </div>
+            <div class="socials__item neo">
+              <a href="#">
+                <img class="social__whatsapp" src="./assets/images/whatsapp.png" alt="">
+              </a>
+            </div>
+          </div>
+        </div>
+        <div class="footer__logo">
+          <img src="./assets/images/LOGO-v1.png" alt="">
+        </div>
+      </div>
+
+      <div class="footer__bottom">
+        <span class="copyright">2023. All right reserved ©</span>
+      </div>
+    </footer>
+  </div>
+  <script src="./assets/scripts/main.js"></script>
+</body>
+
+</html>
